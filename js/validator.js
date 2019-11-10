359ee80bdbed042a520823a6c93d8edd6ca8c748
weru <onewesh@gmail.com>
2019-11-10 22:26:46 +0300

    update

--- js/validator.js
+++ js/validator.js
@@ -1,25 +1,29 @@
 $(function () {
   $('#submitBtn').click(function (event) {
-      event.preventDefault();
-      let input_text = $('#input_text').val();
-      validate(input_text);
+    event.preventDefault();
+    let input_text = $('#input_text').val();
+    validate(input_text);
   });
 });
 function validate(input_text) {
   $.ajax({
-      url: "https://qs1c6q6sc1.execute-api.us-east-1.amazonaws.com/api/validate", data: input_text,
-      contentType: "text/plain", type: "POST", success: update_page
+    url: "https://qs1c6q6sc1.execute-api.us-east-1.amazonaws.com/api/validate", data: input_text,
+    contentType: "text/plain", type: "POST", success: update_page
   });
 }
 function update_page(result) {
+  let output = $('#output');
+  output.addClass('alert');
   if (result.validity === "true") {
-      $('#output').removeClass("alert alert-danger", true);
-      $('#output').addClass("alert alert-success", true);
-      $("#output").html("You entered valid AWS CloudFormation YAML");
+    output
+    .removeClass("alert-danger", true)
+    .addClass("alert-success", true)
+    .html("You entered valid AWS CloudFormation YAML");
   }
   else {
-      $('#output').removeClass("alert alert-success", true);
-      $('#output').addClass("alert alert-danger", true);
-      $("#output").html(result['message']);
+    output
+    .removeClass("alert-success", true)
+    .addClass("alert-danger", true)
+    .html(result['message']);
   }
 }
